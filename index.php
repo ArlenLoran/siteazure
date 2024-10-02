c06105717bfa48b21bbf096dd7338c63c3c7a158
ArlenLoran <108382820+ArlenLoran@users.noreply.github.com>
2024-10-02 16:08:43 -0300

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cadastro de Usuários</title>
+    <title>Cadastro de Usuárioss</title>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 </head>
 <body>
