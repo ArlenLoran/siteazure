4f1c47c25886983c8736c2ff1279373a24f1ed7b
ArlenLoran <108382820+ArlenLoran@users.noreply.github.com>
2024-10-02 14:22:22 -0300

    Update processa.php

--- processa.php
+++ processa.php
@@ -6,14 +6,19 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Conexão com o banco de dados
     $con = mysqli_init();
     mysqli_ssl_set($con, NULL, NULL, "DigiCertGlobalRootG2.crt.pem", NULL, NULL);
-    $conn = mysqli_real_connect($con, "arlendbteste.mysql.database.azure.com", "arlendbteste", "3KT8zx203@Brasil", "tabela1", 3306, MYSQLI_CLIENT_SSL);
-
-    if (!$conn) {
+    
+    // Tente conectar ao banco de dados
+    if (!mysqli_real_connect($con, "arlendbteste.mysql.database.azure.com", "arlendbteste", "3KT8zx203@Brasil", "tabela1", 3306, NULL, MYSQLI_CLIENT_SSL)) {
         die("Erro de conexão: " . mysqli_connect_error());
     }
 
     // Prepara a consulta para evitar injeção de SQL
     $stmt = $con->prepare("INSERT INTO usuarios (nome, senha) VALUES (?, ?)");
+    
+    if ($stmt === false) {
+        die("Erro ao preparar a consulta: " . $con->error);
+    }
+
     $stmt->bind_param("ss", $nome, $senha);
 
     // Executa a consulta
