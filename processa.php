dea80ab6edf594de020747b734303e4ce8d7ce3c
ArlenLoran <108382820+ArlenLoran@users.noreply.github.com>
2024-10-02 14:13:17 -0300

    Create processa.php

---
+++ processa.php
@@ -0,0 +1,30 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nome = $_POST['nome'];
+    $senha = $_POST['senha'];
+
+    // Conexão com o banco de dados
+    $con = mysqli_init();
+    mysqli_ssl_set($con, NULL, NULL, "{path to CA cert}", NULL, NULL);
+    $conn = mysqli_real_connect($con, "arlendbteste.mysql.database.azure.com", "arlendbteste", "3KT8zx203@Brasil", "tabela1", 3306, MYSQLI_CLIENT_SSL);
+
+    if (!$conn) {
+        die("Erro de conexão: " . mysqli_connect_error());
+    }
+
+    // Prepara a consulta para evitar injeção de SQL
+    $stmt = $con->prepare("INSERT INTO usuarios (nome, senha) VALUES (?, ?)");
+    $stmt->bind_param("ss", $nome, $senha);
+
+    // Executa a consulta
+    if ($stmt->execute()) {
+        echo "Usuário cadastrado com sucesso!";
+    } else {
+        echo "Erro: " . $stmt->error;
+    }
+
+    // Fecha a conexão
+    $stmt->close();
+    mysqli_close($con);
+}
+?>
