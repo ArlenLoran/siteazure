424bf1534950e382e739e047ace0d678d298ac22
ArlenLoran <108382820+ArlenLoran@users.noreply.github.com>
2024-10-02 14:10:14 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,20 @@
-<?php
-// Definindo uma variável com a mensagem
-$mensagem = "Bem-vindo ao meu site!";
-
-// Exibindo a mensagem na tela
-echo $mensagem;
-?>
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulário de Cadastro</title>
+</head>
+<body>
+    <h1>Cadastro de Usuário</h1>
+    <form action="processa.php" method="post">
+        <label for="nome">Nome:</label>
+        <input type="text" id="nome" name="nome" required>
+        <br><br>
+        <label for="senha">Senha:</label>
+        <input type="password" id="senha" name="senha" required>
+        <br><br>
+        <input type="submit" value="Cadastrar">
+    </form>
+</body>
+</html>
