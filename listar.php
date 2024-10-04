d7a7507dba8eb3a325fdcfbe82058fc2ad7456c2
ArlenLoran <108382820+ArlenLoran@users.noreply.github.com>
2024-10-04 12:13:52 -0300

    Update listar.php

--- listar.php
+++ listar.php
@@ -1,18 +1,9 @@
 <?php
-// Inicializa a conexão
-$con = mysqli_init();
+require 'db.php';
 
-// Configura o certificado SSL
-$caCertPath = 'DigiCertGlobalRootCA.crt.pem'; // Caminho para o seu certificado CA
-mysqli_ssl_set($con, NULL, NULL, $caCertPath, NULL, NULL);
+try {
+    $con = getDbConnection();
 
-// Realiza a conexão
-$host = 'arlendbteste.mysql.database.azure.com';
-$username = 'arlendbteste';
-$password = '3KT8zx203@Brasil'; // Substitua pelo seu password
-$database = 'tabela1'; // Substitua pelo seu nome do banco de dados
-
-if (mysqli_real_connect($con, $host, $username, $password, $database, 3306, NULL, MYSQLI_CLIENT_SSL)) {
     $query = "SELECT * FROM usuarios";
     $result = mysqli_query($con, $query);
 
@@ -24,9 +15,8 @@ if (mysqli_real_connect($con, $host, $username, $password, $database, 3306, NULL
         echo "Erro na consulta: " . mysqli_error($con);
     }
 
-    // Fecha a conexão
     mysqli_close($con);
-} else {
-    echo "Erro na conexão: " . mysqli_connect_error();
+} catch (Exception $e) {
+    echo $e->getMessage();
 }
 ?>
