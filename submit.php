c670f540cadc120517c2a3669cfdf016b9cf72f2
ArlenLoran <108382820+ArlenLoran@users.noreply.github.com>
2024-10-04 16:18:48 -0300

    Update submit.php

--- submit.php
+++ submit.php
@@ -7,7 +7,7 @@ $name = mysqli_real_escape_string($con, $_POST['name']);
 $email = mysqli_real_escape_string($con, $_POST['email']);
 
 // Insere os dados no banco
-$query = "INSERT INTO usuarios (name, email) VALUES ('$name', '$email')";
+$query = "INSERT INTO usuarios (nome, email) VALUES ('$name', '$email')";
 
 if (mysqli_query($con, $query)) {
     echo "Usuário cadastrado com sucesso!";
