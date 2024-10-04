ba632e9956709caba4ff1cf3408d0666b88dbee9
ArlenLoran <108382820+ArlenLoran@users.noreply.github.com>
2024-10-04 12:26:26 -0300

    Update config.php

--- config.php
+++ config.php
@@ -14,5 +14,6 @@ return [
     'db_host' => $_ENV['DB_HOST'],         // Endereço do servidor MySQL
     'db_username' => $_ENV['DB_USERNAME'], // Nome de usuário do banco de dados
     'db_password' => $_ENV['DB_PASSWORD'], // Senha do banco de dados
-    'db_name' => $_ENV['DB_NAME'],         // Nome do banco de dados
+    'db_database' => $_ENV['DB_DATABASE'], // Nome do banco de dados (ajustado de DB_NAME para DB_DATABASE)
+    'ca_cert_path' => $_ENV['CA_CERT_PATH'], // Caminho para o certificado CA
 ];
