8af6dd5e8e862b93ddaa342da433a6b9395b0bdf
ArlenLoran <108382820+ArlenLoran@users.noreply.github.com>
2024-10-04 12:14:10 -0300

    Update cadastrar.php

--- cadastrar.php
+++ cadastrar.php
@@ -1,24 +1,13 @@
 <?php
-// Inicializa a conexão
-$con = mysqli_init();
+require 'db.php';
 
-// Configura o certificado SSL
-$caCertPath = 'DigiCertGlobalRootCA.crt.pem'; // Caminho para o seu certificado CA
-mysqli_ssl_set($con, NULL, NULL, $caCertPath, NULL, NULL);
+try {
+    $con = getDbConnection();
 
-// Realiza a conexão
-$host = 'arlendbteste.mysql.database.azure.com';
-$username = 'arlendbteste';
-$password = '3KT8zx203@Brasil'; // Substitua pelo seu password
-$database = 'tabela1'; // Substitua pelo seu nome do banco de dados
-
-if (mysqli_real_connect($con, $host, $username, $password, $database, 3306, NULL, MYSQLI_CLIENT_SSL)) {
-    // Verifica se os dados foram enviados
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $nome = mysqli_real_escape_string($con, $_POST['nome']);
         $email = mysqli_real_escape_string($con, $_POST['email']);
 
-        // Insere os dados na tabela
         $query = "INSERT INTO usuarios (nome, email) VALUES ('$nome', '$email')";
         
         if (mysqli_query($con, $query)) {
@@ -28,9 +17,8 @@ if (mysqli_real_connect($con, $host, $username, $password, $database, 3306, NULL
         }
     }
 
-    // Fecha a conexão
     mysqli_close($con);
-} else {
-    echo "Erro na conexão: " . mysqli_connect_error();
+} catch (Exception $e) {
+    echo $e->getMessage();
 }
 ?>
