d9f85e616ad37df95ef65557235a0b17269058bb
ArlenLoran <108382820+ArlenLoran@users.noreply.github.com>
2024-10-04 16:14:38 -0300

    Update submit.php

--- submit.php
+++ submit.php
@@ -1,20 +1,42 @@
 <?php
-// Inclui o arquivo de conexão com o banco de dados
-$con = include 'database.php';
+// Inclui o autoloader do Composer para carregar dependências
+require 'vendor/autoload.php';
 
-// Captura os dados do formulário
-$name = mysqli_real_escape_string($con, $_POST['name']);
-$email = mysqli_real_escape_string($con, $_POST['email']);
+// Inclui o arquivo de configuração
+$config = require 'config.php';
 
-// Insere os dados no banco
-$query = "INSERT INTO usuarios (name, email) VALUES ('$name', '$email')";
+// Inicializa a conexão
+$con = mysqli_init();
 
-if (mysqli_query($con, $query)) {
-    echo "Usuário cadastrado com sucesso!";
+// Configura o certificado SSL
+$caCertPath = 'DigiCertGlobalRootCA.crt.pem'; // Caminho para o seu certificado CA
+mysqli_ssl_set($con, NULL, NULL, $caCertPath, NULL, NULL);
+
+// Realiza a conexão
+$host = $config['db_host'];         // Endereço do servidor MySQL
+$username = $config['db_username']; // Nome de usuário do banco de dados
+$password = $config['db_password']; // Senha do banco de dados
+$database = $config['db_database']; // Nome do banco de dados
+
+if (mysqli_real_connect($con, $host, $username, $password, $database, 3306, NULL, MYSQLI_CLIENT_SSL)) {
+    echo "Conexão bem-sucedida ao banco de dados!<br>";
+    
+    // Captura os dados do formulário
+    $name = mysqli_real_escape_string($con, $_POST['name']);
+    $email = mysqli_real_escape_string($con, $_POST['email']);
+    
+    // Insere os dados no banco
+    $query = "INSERT INTO usuarios (name, email) VALUES ('$name', '$email')";
+    
+    if (mysqli_query($con, $query)) {
+        echo "Usuário cadastrado com sucesso!";
+    } else {
+        echo "Erro ao cadastrar usuário: " . mysqli_error($con);
+    }
+
+    // Fecha a conexão
+    mysqli_close($con);
 } else {
-    echo "Erro ao cadastrar usuário: " . mysqli_error($con);
+    echo "Erro na conexão: " . mysqli_connect_error();
 }
-
-// Fecha a conexão
-mysqli_close($con);
 ?>
