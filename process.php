0e24ba70dbf6fd45c6ee7ee80423aef50a0b73ce
ArlenLoran <108382820+ArlenLoran@users.noreply.github.com>
2024-10-07 19:49:18 -0300

    Update process.php

--- process.php
+++ process.php
@@ -1,54 +1,74 @@
 <?php
-// Supondo que você já tenha uma conexão com o banco de dados estabelecida
-$query = "
-WITH nota_carreta AS (
-    SELECT
-        TRLR_ID,
-        LISTAGG(NOTTXT, '
-        ') WITHIN GROUP (ORDER BY NOTLIN) AS NOTTXT
-    FROM TRLR_NOTE
-    GROUP BY TRLR_ID
-)
-SELECT
-    rci.invnum,
-    tr.trlr_num,
-    dsts.lngdsc AS trlr_stat,
-    tr.trlr_broker,
-    tr.driver_nam,
-    tr.DRIVER_LIC_NUM,
-    dstt.LNGDSC AS trlr_typ,
-    TR.TRLR_ID,
-    ntc.NOTTXT,
-    TR.YARD_LOC,
-    TR.TRACTOR_NUM,
-    TR.TRLR_SEAL1,
-    TR.TRLR_SEAL2,
-    TR.TRLR_SEAL3
-FROM
-    rcvinv rci
-LEFT JOIN RCVtrk rct ON rci.TRKNUM = rct.TRKNUM
-LEFT JOIN trlr tr ON tr.trlr_id = rct.trlr_id
-LEFT JOIN dscmst dsts ON dsts.colval = tr.trlr_stat AND dsts.colnam = 'trlr_stat' AND dsts.locale_id = 'US_ENGLISH'
-LEFT JOIN dscmst dstt ON dstt.colnam = 'trlr_typ' AND dstt.LOCALE_ID = 'US_ENGLISH' AND dstt.colval = tr.trlr_typ
-LEFT JOIN nota_carreta ntc ON ntc.TRLR_ID = tr.trlr_id
-WHERE
-    rci.invnum = '8802889342'
-";
+$url = 'https://score-msc.mysupplychain.dhl.com/score_msc/external/V1/report/160590/run/sync?Content-Type=application%2Fjson&Accept=text%2Fcsv';
 
-// Executar a consulta
-$result = $db->query($query);
+$data = array(
+    'myQuery' => [
+        "WITH nota_carreta AS (
+            SELECT
+                TRLR_ID,
+                LISTAGG(NOTTXT, '
+                ') WITHIN GROUP (ORDER BY NOTLIN) AS NOTTXT
+            FROM TRLR_NOTE
+            GROUP BY TRLR_ID
+        )
+        SELECT
+            rci.invnum,
+            tr.trlr_num,
+            dsts.lngdsc AS trlr_stat,
+            tr.trlr_broker,
+            tr.driver_nam,
+            tr.DRIVER_LIC_NUM,
+            dstt.LNGDSC AS trlr_typ,
+            tr.TRL_ID,
+            ntc.NOTTXT,
+            tr.YARD_LOC,
+            tr.TRACTOR_NUM,
+            tr.TRL_SEAL1,
+            tr.TRL_SEAL2,
+            tr.TRL_SEAL3
+        FROM
+            rcvinv rci
+        LEFT JOIN RCVtrk rct ON rci.TRKNUM = rct.TRKNUM
+        LEFT JOIN trlr tr ON tr.trlr_id = rct.trlr_id
+        LEFT JOIN dscmst dsts ON dsts.colval = tr.trlr_stat AND dsts.colnam = 'trlr_stat' AND dsts.locale_id = 'US_ENGLISH'
+        LEFT JOIN dscmst dstt ON dstt.colnam = 'trlr_typ' AND dstt.LOCALE_ID = 'US_ENGLISH' AND dstt.colval = tr.trlr_typ
+        LEFT JOIN nota_carreta ntc ON ntc.TRLR_ID = tr.trlr_id
+        WHERE
+            rci.invnum = '8802889342'"
+    ],
+    'body' => ['']
+);
 
-if ($result) {
-    // Armazenar os dados em um array
-    $data = [];
-    
-    while ($row = $result->fetch_assoc()) {
-        $data[] = $row;
-    }
-    
-    // Transformar em JSON
-    echo json_encode($data);
+$user = 'arbarret';
+$password = '3KT8zx203@Brasil1';
+$credenciais = $user . ':' . $password;
+$credenciaisBase64 = base64_encode($credenciais);
+
+$headers = array(
+    'Authorization: Basic ' . $credenciaisBase64,
+    'Content-Type: application/json'
+);
+
+// Configurar o contexto HTTP com a requisição POST
+$options = array(
+    'http' => array(
+        'header'  => $headers,
+        'method'  => 'POST',
+        'content' => json_encode($data), // Converte o array de dados para JSON
+        'ignore_errors' => true, // Permite capturar respostas de erro (por exemplo, 400 ou 500)
+    )
+);
+
+$context = stream_context_create($options);
+
+// Fazer a requisição HTTP
+$response = file_get_contents($url, false, $context);
+
+if ($response === FALSE) {
+    // Tratar o erro
+    echo "Erro na requisição";
 } else {
-    echo json_encode(['error' => 'Erro ao executar a consulta.']);
+    // Exibir a resposta
+    echo $response;
 }
 ?>
