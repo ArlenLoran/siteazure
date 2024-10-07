06abce01183cf356398c52f8d811e7a44290433a
ArlenLoran <108382820+ArlenLoran@users.noreply.github.com>
2024-10-07 20:05:59 -0300

    Update process.php

--- process.php
+++ process.php
@@ -2,28 +2,27 @@
 $url = 'https://score-msc.mysupplychain.dhl.com/score_msc/external/V1/report/160590/run/sync';
 
 $data = array(
-    'myQuery' => ["select
-ivs.lodnum,
-ivl.stoloc,
-rcl.prtnum,
-rcl.lotnum,
-rcl.rcvqty,
-'cs' uom,
-prd.LNGDSC descricao,
-rcl.rcvsts,
-ivd.expire_dte,
---peso ta faltando--
-rcl.invnum,
-SUM(rcl.rcvqty) OVER (PARTITION BY rcl.invnum, rcl.prtnum) AS total
+    'myQuery' => ["WITH nota_carreta AS (
+    select
+    TRLR_ID,
+    LISTAGG( NOTTXT, '
+    '  ) WITHIN GROUP (ORDER BY NOTLIN  ) NOTTXT
+    from TRLR_NOTE
+    GROUP BY TRLR_ID
+)
+select
+rci.invnum,
+ tr.trlr_num, dsts.lngdsc trlr_stat, tr.trlr_broker, tr.driver_nam, tr.DRIVER_LIC_NUM, dstt.LNGDSC trlr_typ, TR.TRLR_ID ,
+ ntc.NOTTXT , TR.YARD_LOC,  TR.TRACTOR_NUM, TR.TRLR_SEAL1, TR.TRLR_SEAL2, TR.TRLR_SEAL3
  FROM
-rcvlin rcl
-left join invdtl ivd on rcl.rcvkey =  ivd.rcvkey
-left join invsub ivs on ivs.subnum = ivd.subnum
-left join invlod ivl on ivl.lodnum = ivs.lodnum
-left join prtdsc prd on  prd.locale_id = 'US_ENGLISH' and prd.colval = rcl.prtnum || '|RBCCID|RCKT'
+rcvinv rci
+left join RCVtrk rct on rci.TRKNUM = rct.TRKNUM
+left join trlr tr on tr.trlr_id = rct.trlr_id
+left join dscmst dsts on dsts.colval = tr.trlr_stat and dsts.colnam = 'trlr_stat' and dsts.locale_id = 'US_ENGLISH'
+LEFT JOIN dscmst dstt ON dstt.colnam = 'trlr_typ' and dstt.LOCALE_ID = 'US_ENGLISH' and dstt.colval = tr.trlr_typ
+left join nota_carreta ntc on ntc.TRLR_ID = tr.trlr_id
 where
-invnum = '8802889342'
---trknum, client_id, invnum"],
+rci.invnum = '8802889342'"],
     'body' => ['']
 );
 
