3fa459a5c32a601da85c4cec9990a4fa1eb0dbf7
ArlenLoran <108382820+ArlenLoran@users.noreply.github.com>
2024-10-07 14:26:27 -0300

    Update score.php

--- score.php
+++ score.php
@@ -2,7 +2,28 @@
   $url = 'https://score-msc.mysupplychain.dhl.com/score_msc/external/V1/report/160590/run/sync?Content-Type=application%2Fjson&Accept=text%2Fcsv';
  
   $data = array(
-      'myQuery' => ['SELECT * FROM dual'],
+      'myQuery' => ['select
+ivs.lodnum,
+ivl.stoloc,
+rcl.prtnum,
+rcl.lotnum,
+rcl.rcvqty,
+'cs' uom,
+prd.LNGDSC descricao,
+rcl.rcvsts,
+ivd.expire_dte,
+--peso ta faltando--
+rcl.invnum,
+SUM(rcl.rcvqty) OVER (PARTITION BY rcl.invnum, rcl.prtnum) AS total
+ FROM
+rcvlin rcl
+left join invdtl ivd on rcl.rcvkey =  ivd.rcvkey
+left join invsub ivs on ivs.subnum = ivd.subnum
+left join invlod ivl on ivl.lodnum = ivs.lodnum
+left join prtdsc prd on  prd.locale_id = 'US_ENGLISH' and prd.colval = rcl.prtnum || '|RBCCID|RCKT'
+where
+invnum = '8802889342'
+--trknum, client_id, invnum'],
       'body' => ['']
   );
  
