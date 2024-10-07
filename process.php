8131a621afc21381f459d6de0c6269cacda90371
ArlenLoran <108382820+ArlenLoran@users.noreply.github.com>
2024-10-07 19:52:24 -0300

    Update process.php

--- process.php
+++ process.php
@@ -2,40 +2,31 @@
 $url = 'https://score-msc.mysupplychain.dhl.com/score_msc/external/V1/report/160590/run/sync?Content-Type=application%2Fjson&Accept=text%2Fcsv';
 
 $data = array(
-    'myQuery' => [
-        "WITH nota_carreta AS (
-            SELECT
-                TRLR_ID,
-                LISTAGG(NOTTXT, '
-                ') WITHIN GROUP (ORDER BY NOTLIN) AS NOTTXT
-            FROM TRLR_NOTE
-            GROUP BY TRLR_ID
-        )
-        SELECT
-            rci.invnum,
-            tr.trlr_num,
-            dsts.lngdsc AS trlr_stat,
-            tr.trlr_broker,
-            tr.driver_nam,
-            tr.DRIVER_LIC_NUM,
-            dstt.LNGDSC AS trlr_typ,
-            tr.TRL_ID,
-            ntc.NOTTXT,
-            tr.YARD_LOC,
-            tr.TRACTOR_NUM,
-            tr.TRL_SEAL1,
-            tr.TRL_SEAL2,
-            tr.TRL_SEAL3
-        FROM
-            rcvinv rci
-        LEFT JOIN RCVtrk rct ON rci.TRKNUM = rct.TRKNUM
-        LEFT JOIN trlr tr ON tr.trlr_id = rct.trlr_id
-        LEFT JOIN dscmst dsts ON dsts.colval = tr.trlr_stat AND dsts.colnam = 'trlr_stat' AND dsts.locale_id = 'US_ENGLISH'
-        LEFT JOIN dscmst dstt ON dstt.colnam = 'trlr_typ' AND dstt.LOCALE_ID = 'US_ENGLISH' AND dstt.colval = tr.trlr_typ
-        LEFT JOIN nota_carreta ntc ON ntc.TRLR_ID = tr.trlr_id
-        WHERE
-            rci.invnum = '8802889342'"
-    ],
+    'myQuery' => ["WITH nota_carreta  AS (
+    select
+    TRLR_ID,
+    LISTAGG( NOTTXT, '
+    '  ) WITHIN GROUP (ORDER BY NOTLIN  ) NOTTXT
+ 
+    from TRLR_NOTE
+    --WHERE TRLR_ID = 'TRL0550169'
+    GROUP BY TRLR_ID
+)
+ 
+select
+rci.invnum,
+ tr.trlr_num, dsts.lngdsc trlr_stat, tr.trlr_broker, tr.driver_nam, tr.DRIVER_LIC_NUM, dstt.LNGDSC trlr_typ, TR.TRLR_ID ,
+ ntc.NOTTXT , TR.YARD_LOC,  TR.TRACTOR_NUM, TR.TRLR_SEAL1, TR.TRLR_SEAL2, TR.TRLR_SEAL3
+ FROM
+rcvinv rci
+left join RCVtrk rct on rci.TRKNUM = rct.TRKNUM
+left join trlr tr on tr.trlr_id = rct.trlr_id
+left join dscmst dsts on dsts.colval = tr.trlr_stat and dsts.colnam = 'trlr_stat' and dsts.locale_id = 'US_ENGLISH'
+LEFT JOIN dscmst dstt ON dstt.colnam = 'trlr_typ' and dstt.LOCALE_ID = 'US_ENGLISH' and dstt.colval = tr.trlr_typ
+left join nota_carreta ntc on ntc.TRLR_ID = tr.trlr_id
+where
+rci.invnum = '8802889342'
+--trknum, client_id, invnum"],
     'body' => ['']
 );
 
@@ -55,7 +46,7 @@ $options = array(
         'header'  => $headers,
         'method'  => 'POST',
         'content' => json_encode($data), // Converte o array de dados para JSON
-        'ignore_errors' => true, // Permite capturar respostas de erro (por exemplo, 400 ou 500)
+        'ignore_errors' => true, // Isso permite capturar respostas de erro (por exemplo, 400 ou 500)
     )
 );
 
