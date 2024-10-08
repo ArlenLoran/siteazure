ad3aa1368eb681b4651cc2fe8dfe4de779b92cb7
ArlenLoran <108382820+ArlenLoran@users.noreply.github.com>
2024-10-08 10:40:20 -0300

    Update consulta.php

--- consulta.php
+++ consulta.php
@@ -3,24 +3,25 @@ $url = 'https://score-msc.mysupplychain.dhl.com/score_msc/external/V1/report/160
 
 $data = array(
     'myQuery' => ["WITH nota_carreta AS (
-    SELECT
+    select
     TRLR_ID,
-    LISTAGG(CONCAT('\"', NOTTXT, '\"'), ', ') WITHIN GROUP (ORDER BY NOTLIN) NOTTXT
-    FROM TRLR_NOTE
+    LISTAGG( NOTTXT, '
+    '  ) WITHIN GROUP (ORDER BY NOTLIN  ) NOTTXT
+    from TRLR_NOTE
     GROUP BY TRLR_ID
 )
-SELECT
+select
 rci.invnum,
-tr.trlr_num, dsts.lngdsc trlr_stat, tr.trlr_broker, tr.driver_nam, tr.DRIVER_LIC_NUM, dstt.LNGDSC trlr_typ, TR.TRLR_ID,
-ntc.NOTTXT, TR.YARD_LOC, TR.TRACTOR_NUM, TR.TRLR_SEAL1, TR.TRLR_SEAL2, TR.TRLR_SEAL3
-FROM
+ tr.trlr_num, dsts.lngdsc trlr_stat, tr.trlr_broker, tr.driver_nam, tr.DRIVER_LIC_NUM, dstt.LNGDSC trlr_typ, TR.TRLR_ID ,
+ ntc.NOTTXT , TR.YARD_LOC,  TR.TRACTOR_NUM, TR.TRLR_SEAL1, TR.TRLR_SEAL2, TR.TRLR_SEAL3
+ FROM
 rcvinv rci
-LEFT JOIN RCVtrk rct ON rci.TRKNUM = rct.TRKNUM
-LEFT JOIN trlr tr ON tr.trlr_id = rct.trlr_id
-LEFT JOIN dscmst dsts ON dsts.colval = tr.trlr_stat AND dsts.colnam = 'trlr_stat' AND dsts.locale_id = 'US_ENGLISH'
-LEFT JOIN dscmst dstt ON dstt.colnam = 'trlr_typ' AND dstt.LOCALE_ID = 'US_ENGLISH' AND dstt.colval = tr.trlr_typ
-LEFT JOIN nota_carreta ntc ON ntc.TRLR_ID = tr.trlr_id
-WHERE
+left join RCVtrk rct on rci.TRKNUM = rct.TRKNUM
+left join trlr tr on tr.trlr_id = rct.trlr_id
+left join dscmst dsts on dsts.colval = tr.trlr_stat and dsts.colnam = 'trlr_stat' and dsts.locale_id = 'US_ENGLISH'
+LEFT JOIN dscmst dstt ON dstt.colnam = 'trlr_typ' and dstt.LOCALE_ID = 'US_ENGLISH' and dstt.colval = tr.trlr_typ
+left join nota_carreta ntc on ntc.TRLR_ID = tr.trlr_id
+where
 rci.invnum = '8802889342'"],
     'body' => ['']
 );
@@ -33,7 +34,7 @@ $credenciaisBase64 = base64_encode($credenciais);
 $headers = array(
     'Authorization: Basic ' . $credenciaisBase64,
     'Content-Type: application/json',
-    'Accept: text/csv'
+    'Accept: text/csv' // Aqui, mudamos para text/csv
 );
 
 $options = array(
@@ -52,16 +53,18 @@ $response = file_get_contents($url, false, $context);
 if ($response === FALSE) {
     echo "Erro na requisição";
 } else {
+    // Converter CSV para JSON
     $lines = explode(PHP_EOL, $response);
     $header = str_getcsv(array_shift($lines));
     $result = [];
 
     foreach ($lines as $line) {
         if (!empty($line)) {
-            $result[] = array_combine($header, str_getcsv($line, ',', '"'));
+            $result[] = array_combine($header, str_getcsv($line));
         }
     }
 
+    // Exibir a resposta em formato JSON
     echo json_encode($result);
 }
 ?>
