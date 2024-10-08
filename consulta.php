50e26ce34c2fb80b600abcd2dad6ab102f5a5ec3
ArlenLoran <108382820+ArlenLoran@users.noreply.github.com>
2024-10-08 12:01:10 -0300

    Update consulta.php

--- consulta.php
+++ consulta.php
@@ -6,26 +6,42 @@ $invnum = isset($_POST['invnum']) ? $_POST['invnum'] : '8802889342'; // Valor pa
 
 $data = array(
     'myQuery' => ["WITH nota_carreta AS (
-    select
+    SELECT
     TRLR_ID,
-    LISTAGG( NOTTXT, '
-    '  ) WITHIN GROUP (ORDER BY NOTLIN  ) NOTTXT
-    from TRLR_NOTE
+    LISTAGG(NOTTXT, '
+    ') WITHIN GROUP (ORDER BY NOTLIN) AS NOTTXT
+    FROM TRLR_NOTE
     GROUP BY TRLR_ID
 )
-select
-rci.invnum,
- tr.trlr_num, dsts.lngdsc trlr_stat, tr.trlr_broker, tr.driver_nam, tr.DRIVER_LIC_NUM, dstt.LNGDSC trlr_typ, TR.TRLR_ID ,
- ntc.NOTTXT , TR.YARD_LOC,  TR.TRACTOR_NUM, TR.TRLR_SEAL1, TR.TRLR_SEAL2, TR.TRLR_SEAL3
- FROM
-rcvinv rci
-left join RCVtrk rct on rci.TRKNUM = rct.TRKNUM
-left join trlr tr on tr.trlr_id = rct.trlr_id
-left join dscmst dsts on dsts.colval = tr.trlr_stat and dsts.colnam = 'trlr_stat' and dsts.locale_id = 'US_ENGLISH'
-LEFT JOIN dscmst dstt ON dstt.colnam = 'trlr_typ' and dstt.LOCALE_ID = 'US_ENGLISH' and dstt.colval = tr.trlr_typ
-left join nota_carreta ntc on ntc.TRLR_ID = tr.trlr_id
-where
-rci.invnum = '$invnum'"],
+SELECT
+    rci.invnum,
+    tr.trlr_num, 
+    dsts.lngdsc AS trlr_stat, 
+    tr.trlr_broker, 
+    tr.driver_nam, 
+    tr.DRIVER_LIC_NUM, 
+    dstt.LNGDSC AS trlr_typ, 
+    TR.TRLR_ID,
+    ntc.NOTTXT, 
+    TR.YARD_LOC,  
+    TR.TRACTOR_NUM, 
+    TR.TRLR_SEAL1, 
+    TR.TRLR_SEAL2, 
+    TR.TRLR_SEAL3
+FROM
+    rcvinv rci
+LEFT JOIN 
+    RCVtrk rct ON rci.TRKNUM = rct.TRKNUM
+LEFT JOIN 
+    trlr tr ON tr.trlr_id = rct.trlr_id
+LEFT JOIN 
+    dscmst dsts ON dsts.colval = tr.trlr_stat AND dsts.colnam = 'trlr_stat' AND dsts.locale_id = 'US_ENGLISH'
+LEFT JOIN 
+    dscmst dstt ON dstt.colnam = 'trlr_typ' AND dstt.LOCALE_ID = 'US_ENGLISH' AND dstt.colval = tr.trlr_typ
+LEFT JOIN 
+    nota_carreta ntc ON ntc.TRLR_ID = tr.trlr_id
+WHERE
+    rci.invnum = '$invnum'"],
     'body' => ['']
 );
 
@@ -66,12 +82,12 @@ if ($response === FALSE) {
             $row = array_combine($header, str_getcsv($line));
             // Adiciona apenas ntc.NOTTXT ao resultado
             if (isset($row['NOTTXT'])) {
-                $result[] = $row['NOTTXT'];
+                $result[] = trim($row['NOTTXT']); // Remove espaços em branco
             }
         }
     }
 
     // Exibir a resposta em formato JSON
-    echo json_encode($result);
+    echo json_encode(array_values($result)); // Garante que o array seja reindexado
 }
 ?>
